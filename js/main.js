ca487e3021e461cf74d357bc80dc7ebff3fdaff1
Sam Rae <samuelrae@gmail.com>
2016-01-09 12:35:15 +0000

    added mustache template to each cue so that data comes onto the page at each cue. THen grabbed image data and included it in the mustache template

--- js/main.js
+++ js/main.js
@@ -3,12 +3,23 @@ $(document).ready(function() {
     function addCuePoints(cuePoints) {
         var video = document.getElementsByTagName('video')[0];   
         var track = video.addTextTrack('metadata');
-        $.when(getData("/data/cuepoints.json")).done(function(response){
-            console.log(response);
-            var cuePoints = response.cuepoints.cuepoint;
+        $.when(getData("/data/cuepoints.json"), getData("data/images.json")).done(function(response1, response2){
+            var images = response2[0].images;
+            var cuePoints = response1[0].cuepoints.cuepoint;
+            console.log('images',images);
+            console.log('cuePoints', cuePoints);
             cuePoints.forEach(function(cuePoint, index){
-                var cue = makeCue(cuePoint);
-                track.addCue(cue);
+                function matchImage(element){
+                    return (element.id === cuePoint.image.toString());
+                };
+                if (typeof(cuePoint.stock)!=="undefined" && Number(cuePoint.stock.replace(',','')) >= 100) {
+                    var image = images.find(matchImage);
+                    //console.log(image);
+                    cuePoint.imageLink = image.image;
+                    //console.log(cuePoint.imageLink);
+                    var cue = makeCue(cuePoint);
+                    track.addCue(cue);
+                };
             });
         });
     }
@@ -16,9 +27,10 @@ $(document).ready(function() {
     function makeCue(cuePoint) {
         cuePoint.timeStamp = timeToSeconds(cuePoint.timeStamp);
         console.log(cuePoint.timeStamp);
-        var cue = new VTTCue(cuePoint.timeStamp, cuePoint.timeStamp, cuePoint.desc);
+        var cue = new VTTCue(cuePoint.timeStamp, cuePoint.timeStamp + 2, cuePoint.desc);
         cue.onenter = function() {
             console.log(this.text);
+            renderProductInfo(cuePoint);
         };
         return cue;
     }
@@ -41,9 +53,16 @@ $(document).ready(function() {
       var seconds = (+a[0])*3600 + (+a[1])*60 + (+a[2]);
       return seconds;
     }
-    
+
+
+    function renderProductInfo(product) {
+        var productHTML = Mustache.render("{{desc}},{{price}},<img src='{{imageLink}}'>", product);
+        $('#productBox').html(productHTML);
+    }
+
     addCuePoints();
 
+   
 });
 
 
